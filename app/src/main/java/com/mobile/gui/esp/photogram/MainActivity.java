8688af490dba17343ce3c12f838ed273d65e613a
LeonchikEsp <lav09.01.95@gmail.com>
2016-05-09 20:37:21 +0300

    Примитивное меню, возможность делать фото, фото передается с MainActivity на PlaygroundActivity

--- app/src/main/java/com/mobile/gui/esp/photogram/MainActivity.java
+++ app/src/main/java/com/mobile/gui/esp/photogram/MainActivity.java
@@ -1,13 +1,58 @@
 package com.mobile.gui.esp.photogram;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    TextView info;
+    ImageView iv;
+    Bitmap image;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        info = (TextView) findViewById(R.id.info);
+        iv = (ImageView) findViewById(R.id.photo);
+    }
+
+    protected void takeAPhoto(View view)
+    {
+        info.setText("Take_a_photo button pressed");
+
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, 0);
+    }
+
+    protected void settings(View view)
+    {
+        info.setText("Settings button pressed");
+
+        Intent settingsIntent = new Intent(this, SettingsActivity.class);
+        startActivity(settingsIntent);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK)
+        {
+            image = (Bitmap) data.getExtras().get("data");
+
+            Intent playgroundIntent = new Intent(this, PlaygroundActivity.class);
+            playgroundIntent.putExtra("image", image);
+            startActivity(playgroundIntent);
+        }
     }
 }
---
+++ app/src/main/java/com/mobile/gui/esp/photogram/PlaygroundActivity.java
@@ -0,0 +1,27 @@
+package com.mobile.gui.esp.photogram;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class PlaygroundActivity extends AppCompatActivity
+{
+    ImageView imageView;
+    Bitmap sourceImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playground);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        Intent intent = getIntent();
+        sourceImage = intent.getParcelableExtra("image");
+
+        imageView.setImageBitmap(sourceImage);
+    }
+}
