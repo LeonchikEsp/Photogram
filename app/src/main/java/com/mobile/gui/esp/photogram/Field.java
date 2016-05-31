b78221586f0e6d989ccce48e22e5fc15234c9e87
AndreyFdrv <andrei_hedorov@mail.ru>
2016-05-31 21:44:59 +0300

    Возможность разгадывать нонограмму

--- app/src/main/java/com/mobile/gui/esp/photogram/Field.java
+++ app/src/main/java/com/mobile/gui/esp/photogram/Field.java
@@ -6,20 +6,48 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
  * Created by Andrei on 22.05.2016.
  */
-public class Field extends View {
+public class Field extends View
+{
     private Paint mPaint = new Paint();
     private Rect rect;
     Bitmap image;
-    public Field(Context context, Bitmap image) {
+    Bitmap nonogram;
+    private float square_size=1;
+    private Canvas canvas=null;
+    String type;
+    public void setImage(Bitmap image)
+    {
+        this.image = image;
+    }
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+    public Field(Context context, Bitmap image, String type)
+    {
         super(context);
         this.image=image;
+        this.type=type;
     }
-    private void drawWhiteSquare(Canvas canvas, float x, float y, float side)
+    public float getSize()
+    {
+        return square_size;
+    }
+    public Canvas getCanvas()
+    {
+        return canvas;
+    }
+    public Bitmap getImage()
+    {
+        return image;
+    }
+    public void drawWhiteSquare(Canvas canvas, float x, float y, float side)
     {
         mPaint.setColor(Color.WHITE);
         mPaint.setStyle(Paint.Style.FILL);
@@ -28,7 +56,7 @@ public class Field extends View {
         mPaint.setStyle(Paint.Style.STROKE);
         canvas.drawRect(x, y, x+side, y+side, mPaint);
     }
-    private void drawBlackSquare(Canvas canvas, float x, float y, float side)
+    public void drawBlackSquare(Canvas canvas, float x, float y, float side)
     {
         mPaint.setColor(Color.BLACK);
         mPaint.setStyle(Paint.Style.FILL);
@@ -39,53 +67,55 @@ public class Field extends View {
         mPaint.setColor(Color.BLACK);
         mPaint.setStyle(Paint.Style.STROKE);
         canvas.drawRect(x, y, x + side, y + side, mPaint);
-        mPaint.setTextSize(0.9f*side);
+        mPaint.setTextSize(0.9f * side);
         mPaint.setTextAlign(Paint.Align.CENTER);
-        canvas.drawText(Integer.toString(number), x+side/2, y+3*side/4, mPaint);
+        canvas.drawText(Integer.toString(number), x + side / 2, y + 3 * side / 4, mPaint);
     }
     @Override
-    protected void onDraw(Canvas canvas)
-    {
-        int down_offset=150;
-        float w=getWidth();
-        float h=getHeight();
-        float iw=image.getWidth();
-        float ih=image.getHeight();
-        float square_w=getWidth()/(image.getWidth()+image.getWidth()/2+1);
-        float square_h=(getHeight()-down_offset)/(image.getHeight()+image.getHeight()/2+1);
-        float square_size;
-        if(square_h<square_w)
-            square_size=square_h;
+    protected void onDraw(Canvas canvas) {
+        if(type=="nonogram")
+        {
+            nonogram=Bitmap.createBitmap(image.getWidth(), image.getHeight(), Bitmap.Config.ARGB_8888);
+            for (int i = 0; i < image.getHeight(); i++)
+                for (int j = 0; j < image.getWidth(); j++)
+                {
+                    if (image.getPixel(j, i) == Color.BLACK)
+                        nonogram.setPixel(j, i, Color.BLACK);
+                }
+        }
+        int down_offset = 150;
+        float w = getWidth();
+        float h = getHeight();
+        float iw = image.getWidth();
+        float ih = image.getHeight();
+        float square_w = getWidth() / (image.getWidth() + image.getWidth() / 2 + 1);
+        float square_h = (getHeight() - down_offset) / (image.getHeight() + image.getHeight() / 2 + 1);
+        if (square_h < square_w)
+            square_size = square_h;
         else
-            square_size=square_w;
-        for(int i=0; i<image.getHeight(); i++)//само поле
-            for(int j=0; j<image.getWidth(); j++)
-            {
-                float top_left_x=(image.getWidth()/2+1+j)*square_size;
-                float top_left_y=(image.getHeight()/2+1+i)*square_size;
-                if(image.getPixel(j, i)==Color.BLACK)
+            square_size = square_w;
+        for (int i = 0; i < image.getHeight(); i++)//поле
+            for (int j = 0; j < image.getWidth(); j++) {
+                float top_left_x = (image.getWidth() / 2 + 1 + j) * square_size;
+                float top_left_y = (image.getHeight() / 2 + 1 + i) * square_size;
+                if((image.getPixel(j, i) == Color.BLACK)&&(type!="nonogram"))
                     drawBlackSquare(canvas, top_left_x, top_left_y, square_size);
                 else
                     drawWhiteSquare(canvas, top_left_x, top_left_y, square_size);
             }
-        int black_row_number=0;
-        boolean isPreviousBlack=false;
-        int square_number=1;
-        for(int i=image.getHeight()-1; i>=0; i--)//числа слева от поля
+        int black_row_number = 0;
+        boolean isPreviousBlack = false;
+        int square_number = 1;
+        for (int i = nonogram.getHeight() - 1; i >= 0; i--)//числа слева от поля
         {
-            for (int j = image.getWidth() - 1; j >= 0; j--)
-            {
-                if (image.getPixel(j, i) == Color.BLACK)
-                {
+            for (int j = nonogram.getWidth() - 1; j >= 0; j--) {
+                if (nonogram.getPixel(j, i) == Color.BLACK) {
                     black_row_number++;
                     isPreviousBlack = true;
-                }
-                else
-                {
-                    if (isPreviousBlack)
-                    {
-                        float top_left_x = (image.getWidth() / 2 + 1 - square_number) * square_size;
-                        float top_left_y = (image.getHeight() / 2 + 1 + i) * square_size;
+                } else {
+                    if (isPreviousBlack) {
+                        float top_left_x = (nonogram.getWidth() / 2 + 1 - square_number) * square_size;
+                        float top_left_y = (nonogram.getHeight() / 2 + 1 + i) * square_size;
                         drawNumberSquare(canvas, top_left_x, top_left_y, square_size, black_row_number);
                         square_number++;
                     }
@@ -93,34 +123,30 @@ public class Field extends View {
                     isPreviousBlack = false;
                 }
             }
-            if(isPreviousBlack)
-            {
-                float top_left_x = (image.getWidth() / 2 + 1 - square_number) * square_size;
-                float top_left_y = (image.getHeight() / 2 + 1 + i) * square_size;
+            if (isPreviousBlack) {
+                float top_left_x = (nonogram.getWidth() / 2 + 1 - square_number) * square_size;
+                float top_left_y = (nonogram.getHeight() / 2 + 1 + i) * square_size;
                 drawNumberSquare(canvas, top_left_x, top_left_y, square_size, black_row_number);
             }
-            black_row_number=0;
-            square_number=1;
+            black_row_number = 0;
+            square_number = 1;
             isPreviousBlack = false;
         }
-        black_row_number=0;
-        isPreviousBlack=false;
-        square_number=1;
-        for(int j = image.getWidth() - 1; j >= 0; j--)//числа сверху от поля
+        black_row_number = 0;
+        isPreviousBlack = false;
+        square_number = 1;
+        for (int j = nonogram.getWidth() - 1; j >= 0; j--)//числа сверху от поля
         {
-            for (int i=image.getHeight()-1; i>=0; i--)
-            {
-                if (image.getPixel(j, i) == Color.BLACK)
-                {
+            for (int i = nonogram.getHeight() - 1; i >= 0; i--) {
+                if (nonogram.getPixel(j, i) == Color.BLACK) {
                     black_row_number++;
                     isPreviousBlack = true;
-                }
-                else
+                } else
                 {
                     if (isPreviousBlack)
                     {
-                        float top_left_x = (image.getWidth()/2+1+j)*square_size;
-                        float top_left_y = (image.getHeight() / 2 + 1 - square_number) * square_size;
+                        float top_left_x = (nonogram.getWidth() / 2 + 1 + j) * square_size;
+                        float top_left_y = (nonogram.getHeight() / 2 + 1 - square_number) * square_size;
                         drawNumberSquare(canvas, top_left_x, top_left_y, square_size, black_row_number);
                         square_number++;
                     }
@@ -128,14 +154,14 @@ public class Field extends View {
                     isPreviousBlack = false;
                 }
             }
-            if(isPreviousBlack)
+            if (isPreviousBlack)
             {
-                float top_left_x = (image.getWidth()/2+1+j)*square_size;
-                float top_left_y = (image.getHeight() / 2 + 1 - square_number) * square_size;
+                float top_left_x = (nonogram.getWidth() / 2 + 1 + j) * square_size;
+                float top_left_y = (nonogram.getHeight() / 2 + 1 - square_number) * square_size;
                 drawNumberSquare(canvas, top_left_x, top_left_y, square_size, black_row_number);
             }
-            black_row_number=0;
-            square_number=1;
+            black_row_number = 0;
+            square_number = 1;
             isPreviousBlack = false;
         }
     }
--- app/src/main/java/com/mobile/gui/esp/photogram/activities/PlaygroundActivity.java
+++ app/src/main/java/com/mobile/gui/esp/photogram/activities/PlaygroundActivity.java
@@ -2,31 +2,38 @@ package com.mobile.gui.esp.photogram.activities;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 
 import com.mobile.gui.esp.photogram.BitmapColourToBWConverter;
 import com.mobile.gui.esp.photogram.Field;
 import com.mobile.gui.esp.photogram.FieldGLSurfaceView;
 import com.mobile.gui.esp.photogram.MyGLSurfaceView;
 
-public class PlaygroundActivity extends AppCompatActivity
+public class PlaygroundActivity extends AppCompatActivity implements View.OnTouchListener
 {
     private GLSurfaceView mGLView;
     Bitmap sourceImage;
     Bitmap BWImage;
+    Field field;
+    Bitmap nonogram;
+    Bitmap user_image;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         sourceImage = intent.getParcelableExtra("image");
         BWImage = BitmapColourToBWConverter.convert(sourceImage);
         float size=10;
-        Bitmap nonogram=Bitmap.createBitmap((int)(BWImage.getWidth()/size), (int)(BWImage.getHeight()/size), Bitmap.Config.ARGB_8888);
-        Bitmap test=Bitmap.createBitmap(135, 240, Bitmap.Config.ARGB_8888);
+        nonogram=Bitmap.createBitmap((int)(BWImage.getWidth()/size), (int)(BWImage.getHeight()/size), Bitmap.Config.ARGB_8888);
         for(int i=0; i<=BWImage.getHeight()-size; i+=size)
             for(int j=0; j<=BWImage.getWidth()-size; j+=size)
             {
@@ -40,13 +47,77 @@ public class PlaygroundActivity extends AppCompatActivity
                 if(black_count>size*size/2)
                     nonogram.setPixel((int)(j/size), (int)(i/size), Color.BLACK);
             }
-        setContentView(new Field(this, nonogram));
-
+        user_image=Bitmap.createBitmap(nonogram.getWidth(), nonogram.getHeight(), Bitmap.Config.ARGB_8888);
+        for (int i = 0; i < nonogram.getHeight(); i++)
+            for (int j = 0; j < nonogram.getWidth(); j++)
+            {
+                if (nonogram.getPixel(j, i) == Color.BLACK)
+                    user_image.setPixel(j, i, Color.BLACK);
+            }
+        field=new Field(this, nonogram, "nonogram");
+        field.setOnTouchListener(this);
+        setContentView(field);
+        for (int i = 0; i < user_image.getHeight(); i++)//поле
+            for (int j = 0; j < user_image.getWidth(); j++)
+            {
+                user_image.setPixel(j, i, Color.WHITE);
+            }
         // Create a GLSurfaceView instance and set it
         // as the ContentView for this Activity
-
     }
-
+    @Override
+    public boolean onTouch(View v, MotionEvent event)
+    {
+        float x = event.getX();
+        float y = event.getY();
+        Canvas canvas=new Canvas();
+        float square_size=field.getSize();
+        switch (event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN: //нажатие
+                if(x<(user_image.getWidth()/2+1)*square_size)//левее поля
+                    break;
+                if(x>=(user_image.getWidth()/2+1+user_image.getWidth())*square_size)//правее поля
+                    break;
+                int square_x=0;
+                for(int i=1; i<user_image.getWidth()+1; i++)
+                    if(x<(user_image.getWidth()/2+1+i)*square_size)
+                    {
+                        square_x=i-1;
+                        break;
+                    }
+                if(y<(user_image.getHeight()/2+1)*square_size)//ниже поля
+                    break;
+                if(y>=(user_image.getHeight()/2+1+user_image.getHeight())*square_size)//выше поля
+                    break;
+                int square_y=0;
+                for(int i=1; i<=user_image.getHeight()+1; i++)
+                    if(y<(user_image.getHeight()/2+1+i)*square_size)
+                    {
+                        square_y=i-1;
+                        break;
+                    }
+                if(user_image.getPixel(square_x, square_y) == Color.BLACK)
+                    user_image.setPixel(square_x, square_y, Color.WHITE);
+                else
+                    user_image.setPixel(square_x, square_y, Color.BLACK);
+                field.setImage(user_image);
+                field.setType("user_image");
+                field.setOnTouchListener(this);
+                field.invalidate();
+                setContentView(field);
+                break;
+            /*case MotionEvent.ACTION_MOVE: // движение
+                sMove = "Move: " + x + "," + y;
+                break;
+            case MotionEvent.ACTION_UP: // отпускание
+            case MotionEvent.ACTION_CANCEL:
+                sMove = "";
+                sUp = "Up: " + x + "," + y;
+                break;*/
+        }
+        return true;
+    }
     /*@Override
     protected void onPause() {
         super.onPause();
